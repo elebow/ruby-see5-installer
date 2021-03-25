fb37e5dce9f28cb09c5f93bbe94c98f0618b2814
Eddie Lebow <elebow@users.noreply.github.com>
2021-03-24 23:22:15 -0400

    Add extconf.rb

---
+++ ext/see5-installer/extconf.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "mkmf"
+
+open("Makefile", "w") do |makefile|
+  makefile.puts <<~MAKEFILE
+    make: c50 gritbot
+    c50:
+    	cd ../c5.0 && cat defns.i global.c c50.c construct.c formtree.c info.c discr.c contin.c subset.c prune.c p-thresh.c trees.c siftrules.c ruletree.c rules.c getdata.c implicitatt.c mcost.c confmat.c sort.c update.c attwinnow.c classify.c formrules.c getnames.c modelfiles.c utility.c xval.c \
+      | grep -E -v "defns.i|extern.i" > c50gt.c
+    	cd ../c5.0 && #{cc_config["CC"]} -O3 -o c5.0 c50gt.c -lm
+    	cd ../c5.0 && strip c5.0
+    gritbot:
+    	cd ../gritbot && cat defns.i global.c cluster.c continatt.c outlier.c getdata.c gritbot.c sort.c discratt.c check.c common.c getnames.c implicitatt.c modelfiles.c update.c utility.c \
+      | grep -E -v "defns.i|extern.i" > gritbotgt.c
+    	cd ../gritbot && #{cc_config["CC"]} -O3 -o gritbot gritbotgt.c -lm
+    	cd ../gritbot && strip gritbot
+    install:
+    	# Just leave the executables where they. Other gems can find them there.
+    cleane:
+    	# Any files will be cleaned up when the gem is uninstalled
+  MAKEFILE
+end
